075d4aaba02d706a041442a948c06cac03e1e94f
Adrian1426 <adrianhh1426@gmail.com>
2020-09-02 12:10:39 -0500

    configuración inicial i18next

---
+++ src/config/i18next-config.js
@@ -0,0 +1,19 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n.use(initReactI18next).init({
+  lng: 'es',
+  fallbackLng: 'en',
+  resources: {
+    es: {
+      translation: {
+        title: "Bienvenido a react"
+      }
+    },
+    en: {
+      translation: {
+        title: "Welcome to react"
+      }
+    }
+  }
+});
