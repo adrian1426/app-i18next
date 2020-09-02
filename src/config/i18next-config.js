8f5f1c2ab52b45ee48274d157389f1da629720d4
Adrian1426 <adrianhh1426@gmail.com>
2020-09-02 12:30:02 -0500

    detectando idioma del navegador

--- src/config/i18next-config.js
+++ src/config/i18next-config.js
@@ -1,23 +1,25 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n.use(initReactI18next).init({
-  lng: 'es',
-  fallbackLng: 'en',
-  resources: {
-    es: {
-      translation: {
-        app: {
-          title: "Bienvenido a react <strong>{{name}}</strong>"
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next).init({
+    fallbackLng: 'en',
+    resources: {
+      es: {
+        translation: {
+          app: {
+            title: "Bienvenido a react <strong>{{name}}</strong>"
+          }
         }
-      }
-    },
-    en: {
-      translation: {
-        app: {
-          title: "Welcome to react <strong>{{name}}</strong>"
+      },
+      en: {
+        translation: {
+          app: {
+            title: "Welcome to react <strong>{{name}}</strong>"
+          }
         }
       }
     }
-  }
-});
+  });
