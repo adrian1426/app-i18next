86af73aebd1418d2d312e1e801ad94cca17b3b07
Adrian1426 <adrianhh1426@gmail.com>
2020-09-02 16:18:52 -0500

    i18next - 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import Header from './components/Header';
+import { useTranslation } from 'react-i18next';
 
 function App() {
 
+  const { t, i18n } = useTranslation();
+
   return (
     <div>
-      <Header />
-      <p>Hola mi amigo</p>
+      <p>{t("home.title")}</p>
+      <br />
+      <button onClick={() => i18n.changeLanguage('en')}>En</button>
+      <button onClick={() => i18n.changeLanguage('es')}>Es</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,31 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import i18next from 'i18next';
-import { I18nextProvider } from 'react-i18next';
-import global_es from './translations/es/global.json';
-import global_en from './translations/en/global.json';
-
-i18next.init({
-  lng: 'es',
-  interpolation: {
-    escapeValue: false
-  },
-  resources: {
-    es: {
-      global: global_es
-    },
-    en: {
-      global: global_en
-    }
-  }
-});
+import './i18n';
 
 ReactDOM.render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18next}>
-      <App />
-    </I18nextProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
