ef2c6780c20ad3af0477abb4d775130fa5c542fb
Adrian1426 <adrianhh1426@gmail.com>
2020-09-02 12:37:12 -0500

    imprimiendo idioma y cambiando idioma

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import { useTranslation } from 'react-i18next';
 
 function App() {
 
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const handleChangeIdioma = idioma => {
+    i18n.changeLanguage(idioma);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
+        <h2>idioma: {i18n.language}</h2>
         <p>
           {t('app.title', { name: 'Adriancito' })}
         </p>
         <p dangerouslySetInnerHTML={{ __html: t('app.title', { name: "Adriancito" }) }} />
+
+        <br />
+        <button onClick={() => handleChangeIdioma('en')}>English</button>
+        <button onClick={() => handleChangeIdioma('es')}>Español</button>
       </header>
     </div>
   );
